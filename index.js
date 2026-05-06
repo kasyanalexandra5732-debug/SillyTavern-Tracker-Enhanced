748dadeb46fda5cededd34636d15a674b751ba4f
Anon <kasyan.alexandra5732@gmail.com>
2026-05-06 22:16:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ eventSource.on(event_types.CHAT_CHANGED, eventHandlers.onChatChanged);
 eventSource.on(event_types.CHARACTER_MESSAGE_RENDERED, eventHandlers.onCharacterMessageRendered);
 eventSource.on(event_types.USER_MESSAGE_RENDERED, eventHandlers.onUserMessageRendered);
 eventSource.on(event_types.GENERATION_AFTER_COMMANDS, eventHandlers.onGenerateAfterCommands);
-eventSource.on(event_types.CHAT_COMPLETION_PROMPT_READY, eventHandlers.generateAfterCombinePrompts);
+eventSource.on(event_types.CHAT_COMPLETION_PROMPT_READY, eventHandlers.onChatCompletionPromptReady);
 
 
 SlashCommandParser.addCommandObject(SlashCommand.fromProps({
